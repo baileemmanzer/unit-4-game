2a829d624680219c8af9862eec6625d72a4fd197
Bailee Manzer <baileemanzer3@gmail.com>
2019-10-25 17:04:16 -0600

    set up score counts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
-//  This code will run as soon as the page loads.
-window.onload = function() {
-    $("#lap").on("click", recordLap);
-    $("#stop").on("click", stop);
-    $("#reset").on("click", reset);
-    $("#start").on("click", start);
-  };
+//Selects a random number b/w 19 and 120 at the start of each game 
+var Random=Math.floor(Math.random()*101+19)
+//Displays the random number to the randomnNumber in HTML
+$('#randomNumber').text(Random);
+
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
